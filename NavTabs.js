85b1d38944f5f07038daae4f89fd2d3e31cffa60
pirhaseeb1 <ahmad.haseeb55@gmail.com>
2022-04-21 07:20:41 +0500

    CypressCommitted

--- NavTabs.js
+++ NavTabs.js
@@ -2,9 +2,13 @@
 class NavTabs{
 
     gotoTab(tab){
-        let text='//a[contains(text(),"' + tab + '")]';
+        let text='//a[contains(text(),"' + tab + '")]/..';
         cy.xpath(text).click();
+        cy.wait(500);
 
+        // assertions
+        cy.xpath(text).should('have.class', 'active'); 
+        cy.get('.pagination').find('li').find('a').should('have.attr','disabled', 'disabled');
  
     }
 
--- pomDemo.spec.js
+++ pomDemo.spec.js
@@ -2,28 +2,19 @@
 import LoginPage from '../StepEx/loginPage'
 import NavTabs from '../StepEx/NavTabs'
 
-    it("Login with valid credentials", function () {
+    it("Login with valid credentials and Navigate the Step tabs", function () {
         const login = new LoginPage();
         login.navigate();
         login.login_username('admin');
         login.login_pswd('init');
         login.login_click();
-        
-});
-
-    it("Navigation Bar", function(){
         const ntab = new NavTabs();
         ntab.gotoTab("Keywords");
-        cy.url().should('contain', 'http://localhost:8080/#/root/functions')
         ntab.gotoTab("Parameters");
-        cy.url().should('contain', 'http://localhost:8080/#/root/parameters')
         ntab.gotoTab("Executions");
-        cy.url().should('contain', 'http://localhost:8080/#/root/executions')
         ntab.gotoTab("Scheduler");
-        cy.url().should('contain', 'http://localhost:8080/#/root/schedular')
         ntab.gotoTab("Grid");
-        cy.url().should('contain', 'http://localhost:8080/#/root/grid')
         ntab.gotoTab("Admin");
-        cy.url().should('contain', 'http://localhost:8080/#/root/admin')
 
 });
+ 
